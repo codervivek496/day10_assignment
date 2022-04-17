2fb7d24d14fe5af5ef9b5662a5bf48191a2dd030
codervivek496 <viveksinghssm496@gmail.com>
2022-04-17 17:46:25 +0530

    uc6

--- day10pb1.sh
+++ day10pb1.sh
@@ -1,6 +1,7 @@
 #Arithmetic computation and sorting
 
 #! /bin/bash -x
+declare -A computation
 read -p "Enter valur of a: " a
 read -p "Enter value of b: " b
 read -p "Enter value of c: " c
@@ -10,7 +11,14 @@ uc3=`expr $a \* $b + $c`
 uc4=`expr $c + $b / $b`
 uc5=`expr $a % $b + $c`
 
-echo "UC2= $uc2"
-echo "UC3= $uc3"
-echo "UC4= $uc4"
-echo "UC5= $uc5"
+#echo "UC2= $uc2"
+#echo "UC3= $uc3"
+#echo "UC4= $uc4"
+#echo "UC5= $uc5"
+
+computation=(["UC2"]=$uc2 ["US3"]=$uc3 ["UC4"]=$uc4 ["UC5"]=$uc5)
+
+for comp in "${!computation[@]}";
+do
+	echo $comp - ${computation[$comp]};
+done
