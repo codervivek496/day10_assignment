41339d2c55439097cc0b492fc75b7d19f214a115
codervivek496 <viveksinghssm496@gmail.com>
2022-04-17 16:18:31 +0530

    uc5

--- day10pb1.sh
+++ day10pb1.sh
@@ -8,7 +8,9 @@ read -p "Enter value of c: " c
 uc2=`expr $a + $b \* $c`
 uc3=`expr $a \* $b + $c`
 uc4=`expr $c + $b / $b`
+uc5=`expr $a % $b + $c`
 
 echo "UC2= $uc2"
 echo "UC3= $uc3"
-echo "UC4 = $uc4"
+echo "UC4= $uc4"
+echo "UC5= $uc5"
