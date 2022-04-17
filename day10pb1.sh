c454254bb81ed50ddf767b26b1508ba6a5f40785
codervivek496 <viveksinghssm496@gmail.com>
2022-04-17 15:51:49 +0530

    uc3

--- day10pb1.sh
+++ day10pb1.sh
@@ -5,5 +5,7 @@ read -p "Enter valur of a: " a
 read -p "Enter value of b: " b
 read -p "Enter value of c: " c
 
-uc2=$(( $a + $b * $c ))
+uc2=`expr $a + $b \* $c`
+uc3=`expr $a \* $b + $c`
 echo "UC2= $uc2"
+echo "UC3= $uc3"
